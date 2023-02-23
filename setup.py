c3d4338af78863e95c06bb6eea724dfce90e947a
Qihua <zhongqihua@gmail.com>
2023-02-23 17:42:54 -0500

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-	setup(name='stockpyl', packages=['stockpyl'])
+	setup(name='stockpyl',
+		  version='0.0.14',
+		  py_modules=['stockpyl'],
+		  )
